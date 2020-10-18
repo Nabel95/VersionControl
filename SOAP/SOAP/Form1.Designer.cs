2b1dcbc603472538c2fc0d71a605cea40a4d6475
Abel Nagy <abel.nagy1@stud.uni-corvinus.hu>
2020-10-18 22:58:51 +0200

    5.XML process

--- SOAP/SOAP/Form1.Designer.cs
+++ SOAP/SOAP/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(490, 459);
+            this.dataGridView1.Size = new System.Drawing.Size(575, 461);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
--- SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/Form1.cs
@@ -10,22 +10,60 @@ using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SOAP
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates;
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
         public Form1()
         {
             InitializeComponent();
-            GetExchangeRates();
-            
-            dataGridView1.DataSource = Rates;
+            //GetExchangeRates();
 
+
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            dataGridView1.DataSource = Rates;
+            XmlProcess(result);
         }
 
-        private static void GetExchangeRates()
+        private void XmlProcess(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        public static void GetExchangeRates()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -39,5 +77,8 @@ namespace SOAP
 
             var result = response.GetExchangeRatesResult;
         }
+
+
+
     }
 }
