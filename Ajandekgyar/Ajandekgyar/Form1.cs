d78862c2b6a475be3c03d30dbd1a44ee8ca2ff11
Abel Nagy <abel.nagy1@stud.uni-corvinus.hu>
2020-11-15 23:05:29 +0100

    13. Coloring the ball

--- Ajandekgyar/Ajandekgyar/Form1.cs
+++ Ajandekgyar/Ajandekgyar/Form1.cs
@@ -64,7 +64,10 @@ namespace Ajandekgyar
 
         private void btnSelectBall_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory()
+            {
+                BallColor = btnColor.BackColor
+            };
         }
 
         private void DisplayNext()
